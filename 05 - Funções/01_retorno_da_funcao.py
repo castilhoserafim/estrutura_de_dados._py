1dbc61499540f20b4148e3fcdbecc1c7e3dbdbf3
castilhoserafim <wesleycserafim@gmail.com>
2026-01-15 18:46:57 -0300

    Update 01_retorno_da_funcao.py

--- 05 - Funções/01_retorno_da_funcao.py
+++ 05 - Funções/01_retorno_da_funcao.py
@@ -9,5 +9,9 @@ def retorna_antecessor_e_sucessor(numero):
     return antecessor, sucessor
 
 
+def func_3():
+    print("Olá mundo!")
+
 print(calcular_total([10, 20, 34]))  # 64
 print(retorna_antecessor_e_sucessor(10))  # (9, 11)
+print(func_3())  # None 
