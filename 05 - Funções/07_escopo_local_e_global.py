d6d43302abb5e67f38fed35a7761cdd9709649de
castilhoserafim <wesleycserafim@gmail.com>
2026-01-15 19:17:01 -0300

    Update 07_escopo_local_e_global.py

--- 05 - Funções/07_escopo_local_e_global.py
+++ 05 - Funções/07_escopo_local_e_global.py
@@ -3,8 +3,16 @@ salario = 2000
 
 def salario_bonus(bonus):
     global salario
+
+    lista_aux = lista.copy()
+    lista_aux.append(2)
+    print(f"lista aux={lista_aux}")
+
     salario += bonus
     return salario
 
 
-salario_bonus(500)  # 2500
+lista = [1]
+salario_com_bonus = salario_bonus(500)  # 2500
+print(salario_com_bonus)
+print(lista)
